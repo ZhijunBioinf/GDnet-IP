20254610d56c2a1c98322f2ef0b95e7b23067aef
Zhijun (Walter) Dai <71756048+ZhijunBioinf@users.noreply.github.com>
2025-03-20 19:16:58 +0800

    Update GDnet_IP.py

--- GDnet_IP.py
+++ GDnet_IP.py
@@ -1,139 +1,165 @@
-import random
-import WeDIV2
-import torch.nn as nn
-import torch
+##################################################################################
+# Performing 3-kold cross validation using GDnet-IP model
+#
+# Authors: 
+#   - Dongcheng Li (dongchengli287@gmail.com)
+#   - Zhijun Dai (daizhijun@hunau.edu.cn)
+# Date Created: 09-01-2024
+# Version: 1.0
+#
+# Usage: python GDnet_IP_kfold.py
+#
+# Dependencies:
+#   - Python >= 3.x
+#   - Required Libraries: numpy, collections, scipy, pandas, matplotlib, math, torchvision, torch, PIL
+#
+#
+##################################################################################
+
+import os, random, time, prepData, defGDnet
 import numpy as np
+from collections import Counter
+
+import torchvision, torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+from torch.optim.lr_scheduler import CosineAnnealingLR ,MultiStepLR
+
+
+def set_seed(seed): # Define the seed of random numbers
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+def extract(predict, label): # Extract the predicted and observed labels
+    list_p = []
+    list_b = []
+    list_p = [i.item() for i in predict]
+    list_b = [i.item() for i in label]
+    return list_p, list_b
+
+def evaluate(y_prediction, label, num_class): # Calculate the evaluation measure (Balanced Accuracy)
+    acc = []
+    for cla in range(num_class):
+        cla_num = 0
+        inde = 0
+        right = 0
+        for i in label:
+            if i == cla:
+                cla_num += 1 
+        for i in label:
+            if i == cla:
+                start = inde
+                end = start + cla_num
+                break
+            else:
+                inde += 1
+        y = y_prediction[start:end]
+        for j in y:
+            if j == cla:
+                right += 1
+        acc.append((right / cla_num))
+    return sum(acc) / num_class
+
+def kfold3(seed, root_path, epoch_num=43, k=[1,2,3]): # Perform 3-fold cross validation similar to CPAFNet
+    for fold in k:
+        set_seed(seed)
+        start_time = time.time()
+        ba_list = []
+        file_name = f'defGDnet_{random}_{seed}_{fold}.txt'
+        device = torch.device("cuda:0")
+        print(device)
+        net = defGDnet.GDnet("V1")
+        net.to(device)
+        loss_entropy = nn.CrossEntropyLoss()
+        optimizar = optim.Adam(net.parameters(), lr=0.0008)
+        is_run = 2
+
+        train_images_path,train_images_label,val_images_path,val_images_label = prepData.read_data(root_path,fold=fold)
+        train_images_path = [path for path in train_images_path if not path.endswith('.ipynb_checkpoints')]
+        train_dataset = prepData.Mydata(image_path=train_images_path, images_class=train_images_label, transform=True)
+        train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=10)
+
+        val_images_path = [path for path in val_images_path if not path.endswith('.ipynb_checkpoints')]
+        val_dataset = prepData.valMydata(image_path=val_images_path, images_class=val_images_label, transform=True)
+        val_loader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=10)
+
+        iteration_times = 0
+        a = 0
+        index = 0
+        best_acc = 0.0
+        acc = 0.0
+        acc_list = []
+        loss_list=[]
+        c = -1
+        for epoch in range(1,epoch_num+1):
+            net.train()
+            if epoch >= 2:
+                index += 1
+                if index > 3:
+                    index -= 3
+                    a += 1
+                if a % 2 != 0:
+                    optimizar.param_groups[0]["lr"] = 0.0008*0.1
+                if a % 2 == 0:
+                    optimizar.param_groups[0]["lr"] = 0.0008
+            for step, data in enumerate(train_loader):
+                img, label = data
+                optimizar.zero_grad()
+                output = net(img.to(device), label.to(device))
+                output = output.reshape(-1, 20)
+                loss = loss_entropy(output, label.to(device))
+                loss.backward()
+                optimizar.step()
+                loss_list.append(loss.item())
+                iteration_times += 1
+                if iteration_times % 10 == 0:
+                    print("Times of training: {}; Current loss: {}".format(iteration_times, loss.item()))
+                    print("Epoch: {}".format(epoch))
+            if a %2 != 0 and epoch > 9:
+                acc = 0.0
+                with torch.no_grad():
+                    net.eval() # validation
+                    list_p, list_b = [], []
+                    for img, label in val_loader:
+                        img = img.to(device)
+                        output = net(img, label)
+                        output = output.reshape(-1, 20)
+                        predict = torch.max(output, dim=1)[1]  
+                        list_temp_p, list_temp_b = extract(predict, label)
+                        list_p = list_p + list_temp_p
+                        list_b = list_b + list_temp_b
+                        acc += (predict == label.to(device)).sum().item()
+                acc=acc/1484
+                acc_list.append(acc)
+                balanced_accuracy = evaluate(list_p, list_b, num_class=20)
+                print("BC is :{}".format(balanced_accuracy))
+                print("AC is :{}".format(acc))
+                ba_list.append(balanced_accuracy)
+                print(balanced_accuracy)
+
+        balanced_accuracy=sum(ba_list)/len(ba_list)
+        fold_time = time.time() - start_time
+        with open(file_name, 'w') as file:
+            file.write("BC List: ")
+            for bc in ba_list:
+                file.write(f"{bc} ")
+            file.write("Fold Times: ")
+            file.write(f"{fold_time} ")
+            file.write("\n")
+            file.write(f"balanced_accuracy: {balanced_accuracy}\n")
+        file.close()
+
 
 device = torch.device("cuda:0")
-def GDnet(name, **kwargs):  # Factory function to create a GDnet-IP model
-    model = GDnetIP(vgg_structure(cfgs[name]), class_num=20)
-    return model
+print(device)
+root_path = './py/data/train_data'
+insect_class = [i for i in listdir(root_path) if isdir(joinPath(root_path, i))]
+class_index = dict((k, v) for v, k in enumerate(insect_class))
 
-class GDnetIP(nn.Module):
-    def __init__(self, feature_net, class_num=20):
-        super(GDnetIP, self).__init__()
-        self.feature_net = feature_net
-        self.inception = Inception(in_channel=256)  # 256
-        self.bn1 = nn.Sequential(nn.PReLU(), nn.BatchNorm2d(256))
-        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.inception2 = Inception(in_channel=256)
-        self.bn2 = nn.Sequential(nn.PReLU(), nn.BatchNorm2d(256))
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
-        self.inception3 = Inception(in_channel=256)
-        self.bn3 = nn.Sequential(nn.PReLU(), nn.BatchNorm2d(256))
-        self.av = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=class_num, kernel_size=1))
-        self._initialize_wight()
-
-    # For clustering-based GDnet-IP (default): inception_prob=[1.0,1.0,1.0], is_wediv=1
-    # For inception-based GDnet-IP: [prob,prob,prob], is_wediv=0
-    def forward(self, x, inception_prob=[1.0,1.0,1.0], is_wediv=1):    
-        x = self.feature_net(x)
-        x = self.inception(x, inception_prob[0])
-        x = self.bn1(x)
-        x = self.pool1(x)
-        x = self.inception2(x, inception_prob[1])
-        x = self.bn2(x)
-        x = self.pool2(x)
-        x = self.inception3(x, inception_prob[2])
-        x = self.bn3(x)
-        x = self.av(x)
-        if is_wediv == 1:
-            x = self.wediv(x)
-
-        x = self.classifier(x)
-        return x
-
-
-    def _initialize_wight(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.xavier_uniform(m.weight)
-                if m.bias is not None:
-                    nn.init.constant(m.bias, 0)
-
-
-class Inception(nn.Module): # Inception module
-    def __init__(self, in_channel):
-        super(Inception, self).__init__()
-        self.branch1 = nn.Conv2d(in_channel, out_channels=64, kernel_size=1)
-
-        self.branch2 = nn.Sequential(nn.Conv2d(in_channel, out_channels=64, kernel_size=1),
-                                     nn.Conv2d(64, out_channels=96, kernel_size=3, padding=1))
-        self.branch3 = nn.Sequential(nn.Conv2d(in_channel, out_channels=16, kernel_size=1),
-                                     nn.Conv2d(16, out_channels=64, kernel_size=3, padding=1))
-        self.branch4 = nn.Sequential(nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-                                     nn.Conv2d(in_channel, out_channels=32, kernel_size=1))
-
-    def forward(self, x, inception_prob):
-        self.bernouli1 = torch.distributions.bernoulli.Bernoulli(torch.tensor([inception_prob]))
-        if self.training:
-            branch1 = self.branch1(x)
-            branch2 = self.branch2(x)
-            branch3 = self.branch3(x)
-            branch4 = self.branch4(x)
-            if torch.equal(self.bernouli1.sample(), torch.ones(1)) != 1:
-                branch1=torch.zeros_like(branch1)
-            if torch.equal(self.bernouli1.sample(), torch.ones(1)) != 1:
-                branch2 = torch.zeros_like(branch2)
-            if torch.equal(self.bernouli1.sample(), torch.ones(1)) != 1:
-                branch3 = torch.zeros_like(branch3)
-            if torch.equal(self.bernouli1.sample(), torch.ones(1)) != 1:
-                branch4 = torch.zeros_like(branch4)
-            x = [branch1, branch2, branch3, branch4]
-
-            output = torch.cat(x, 1)
-            return output
-
-        else:
-            branch1 = self.branch1(x)
-            branch2 = self.branch2(x)
-            branch3 = self.branch3(x)
-            branch4 = self.branch4(x)
-            x = [branch1, branch2, branch3, branch4]
-            output = torch.cat(x, 1)
-            return output
-
-cfgs = {"V1": ["M", 128, "M", 256, "M", 256, "M"]}  # Convolution layers kernel size
-
-def vgg_structure(cfg: list):
-    layer = []
-    layer.append(nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1))
-    layer.append(nn.PReLU())  # set PReLU
-    in_channel = 64
-    for i in cfg:
-        if i == "M":
-            layer.append(nn.MaxPool2d(kernel_size=2, stride=2))
-        else:
-            con_2d = nn.Conv2d(in_channel, out_channels=i, kernel_size=3, stride=1, padding=1)
-            layer.append(con_2d)
-            layer += [nn.PReLU()]
-            in_channel = i
-    return nn.Sequential(*layer)
-
-class Wediv(nn.Module):
-    def __init__(self, size):
-        super(Wediv, self).__init__()
-        self.berbouli = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.5]))
-        self.mask = torch.ones(1, size)
-    def forward(self, x):
-        if self.training:
-            self.indentity = x.detach().cpu().squeeze().numpy().T
-            Y_CL, K_optimal, W_optimal, rch = WeDIV2.WeDIV2(self.indentity, w_step=0.2, KList=[4])
-            group_indices = {}
-            self.mask = self.mask.to(device)
-            for group_label in range(K_optimal):
-                group_indices[group_label] = [i for i, label in enumerate(Y_CL) if label == group_label]
-            for group in group_indices:
-                if torch.equal(self.berbouli.sample(), torch.ones(1)) != 1:
-                    self.mask[0, group_indices[group]] = 0
-            result = (x * self.mask.unsqueeze(2).unsqueeze(3))
-            self.mask = torch.ones(1, 256)
-            return result
-        else:
-            return x
-
-if __name__ == '__main__':
-    net = GDnet("V1")
-
+for i in [1,2,3]:
+    kfold3(i ,root_path=root_path)
